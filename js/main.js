9acc25b5e79561a0740959ecfb7b7b5467dab1c3
Denis Berezin <nocab139@yandex.ru>
2021-07-09 01:01:09 +0300

    #: fix

--- js/main.js
+++ js/main.js
@@ -16,19 +16,13 @@ async function getCurrencies() {
     const response = await fetch('https://www.cbr-xml-daily.ru/daily_json.js');
     const data = await response.json();
 
-
     rates.USD = data.Valute.USD;
-
     rates.EUR = data.Valute.EUR;
-
     rates.GBP = data.Valute.GBP;
 
-    console.log(rates)
 
     elementUSD.textContent = rates.USD.Value.toFixed(2);
-
     elementEUR.textContent = rates.EUR.Value.toFixed(2);
-
     elementGBP.textContent = rates.GBP.Value.toFixed(2);
 
     // Цвет для информера USD
